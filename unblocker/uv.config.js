b81fda8ff5798eb6d290a0cec1fa05ba2961bb58
XLDANIEL247 <adelacruz2021delacruz@gmail.com>
2026-03-11 13:46:33 -0400

    Create uv.config.js

---
+++ unblocker/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+  prefix: '/Math-notes/unblocker/service/',
+  bare: 'https://uv.holyubofficial.net/',
+  encodeUrl: Ultraviolet.codec.xor.encode,
+  decodeUrl: Ultraviolet.codec.xor.decode,
+  handler: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@2.6.3/dist/uv.handler.js',
+  bundle: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@2.6.3/dist/uv.bundle.js',
+  config: '/Math-notes/unblocker/uv.config.js',
+  sw: '/Math-notes/unblocker/uv.sw.js',
+};
