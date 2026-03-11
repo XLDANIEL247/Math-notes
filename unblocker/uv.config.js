ccc14405aecc66a12e69159c02435ab86d4d2dac
XLDANIEL247 <adelacruz2021delacruz@gmail.com>
2026-03-11 14:13:33 -0400

    Update uv.config.js

--- unblocker/uv.config.js
+++ unblocker/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: '/Math-notes/unblocker/service/',
-  bare: 'https://uv.holyubofficial.net/',
+  bare: 'https://quiet-moon-c670.adelacruz2021delacruz.workers.dev/',
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: 'https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@2.6.3/dist/uv.handler.js',
